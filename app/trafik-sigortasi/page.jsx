b7d599172129f4331f3c90668753f28be4cccf4d
Miraç <ahmetmiracbakir@protonmail.com>
2023-08-25 17:52:14 +0300

    update

--- app/trafik-sigortasi/page.jsx
+++ app/trafik-sigortasi/page.jsx
@@ -1,17 +1,18 @@
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { trafik } from "@/app/utils/yup";
+import { TrafikSchema } from "@/app/utils/yup";
 const Page = () => {
   const {
+    setValue,
     register,
     handleSubmit,
     watch,
     formState: { errors },
     reset,
-  } = useForm({ mode: "onTouched" });
+  } = useForm({ mode: "onTouched", resolver: yupResolver(TrafikSchema) });
 
   const onSubmit = (data) => {
     console.log({ data });
@@ -22,83 +23,105 @@ const Page = () => {
     <div className="p-4 sm:p-8">
       <HeaderSection />
       <div className="h-screen max-w-6xl mx-auto mt-12">
-        {/* Steps Start */}
-        <React.Fragment>
-          <div className="space-y-5">
-            <div>
-              <h5 className="text-2xl font-bold tracking-tight text-sky-950">
-                1. Nasıl teklif almak istersin?
-              </h5>
-              <p className="mt-1 font-normal text-slate-400">
-                İstediğin teklif alma seçeneğini seç! Yakında daha fazlası
-                eklenecek.
-              </p>
-            </div>
-            <button className="flex flex-col gap-4 py-5 pl-6 pr-6 border xs:flex-row xs:pl-6 xs:pr-12 border-sky-500 active:scale-95 rounded-xl bg-sky-500">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="self-center w-7 h-7 stroke-white fill-white/20"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <div>
-                <span className="block font-semibold text-white">
-                  Form ile teklif almak istiyorum.
-                </span>
-                <span className="block mt-1 text-sm text-left text-slate-100/75">
-                  SMS doğrulaması yapılır.
-                </span>
-              </div>
-            </button>
-          </div>
-          <hr className="my-8" />
-          <div>
-            <h5 className="text-xl font-bold">
-              Kişisel Bilgilerine ihtiyacımız var
-            </h5>
-            <p className="mt-1 text-slate-400">
-              Seni tanımamıza izin ver, merak etme verilerin güvende!
-            </p>
-          </div>
-        </React.Fragment>
-        {/* Steps End */}
-
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="grid max-w-2xl grid-cols-2 gap-4 mt-6 sm:flex-nowrap"
+          className="grid max-w-2xl grid-cols-2 gap-4 mt-6 space-y-5 sm:flex-nowrap"
         >
+          {/* Steps Start */}
+          <div className="col-span-full">
+            <div className="space-y-5">
+              <div>
+                <h5 className="text-2xl font-bold tracking-tight text-sky-950">
+                  1. Nasıl teklif almak istersin?
+                </h5>
+                <p className="mt-1 font-normal text-slate-400">
+                  İstediğin teklif alma seçeneğini seç! Yakında daha fazlası
+                  eklenecek.
+                </p>
+              </div>
+              <button className="flex flex-col gap-4 py-5 pl-6 pr-6 border xs:flex-row xs:pl-6 xs:pr-12 border-sky-500 active:scale-95 rounded-xl bg-sky-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="self-center w-7 h-7 stroke-white fill-white/20"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <div>
+                  <span className="block font-semibold text-white">
+                    Form ile teklif almak istiyorum.
+                  </span>
+                  <span className="block mt-1 text-sm text-left text-slate-100/75">
+                    SMS doğrulaması yapılır.
+                  </span>
+                </div>
+              </button>
+            </div>
+            <hr className="my-8" />
+            <div>
+              <h5 className="text-xl font-bold">
+                Kişisel Bilgilerine ihtiyacımız var
+              </h5>
+              <p className="mt-1 text-slate-400">
+                Seni tanımamıza izin ver, merak etme verilerin güvende!
+              </p>
+            </div>
+          </div>
+          {/* Steps End */}
           <label
             htmlFor="fullName"
             className="flex-grow block col-span-full xs:col-span-1"
           >
             <input
               id="fullName"
-              type="email"
+              name="fullName"
+              type="text"
               className={`${
                 errors.fullName
                   ? "bg-red-50 border-red-400 focus:border-red-500 placeholder:text-red-500 focus:bg-red-100"
-                  : "bg-slate-50/50  border-slate-200/70 focus:bg-slate-50/20 focus:border-slate-300"
-              } p-4 border outline-none rounded-xl w-full max-w-sm transition-all`}
-              {...register("fullName", {
-                required: "Bu alan boş bırakılamaz.",
-                pattern: {
-                  value: /[^a-zA-ZwığüşöçĞÜŞÖÇİ]/g,
-                  message: "Bu alana sayı giremezsin.",
-                },
-              })}
+                  : "bg-slate-50/50 border-slate-200/70 focus:bg-slate-50/20 focus:border-slate-300"
+              } p-4 uppercase border placeholder:capitalize outline-none hover:border-slate-200 rounded-xl w-full max-w-sm transition-all`}
+              {...register("fullName")}
               placeholder="Ad ve Soyad"
               autoComplete="off"
+              maxLength={50}
+              onKeyDown={(e) => {
+                if (/^[0-9]/.test(e.key)) {
+                  e.preventDefault();
+                }
+              }}
             />
             <span className="block mt-1.5 text-sm md:text-base text-red-600">
-              {errors.fullName && errors.fullName.message}
+              {errors.fullName?.message}
+            </span>
+          </label>
+          <label
+            htmlFor="identityNumber"
+            className="flex-grow block col-span-full xs:col-span-1"
+          >
+            <input
+              id="identityNumber"
+              name="identityNumber"
+              type="number"
+              className={`${
+                errors.identityNumber
+                  ? "bg-red-50 border-red-400 focus:border-red-500 placeholder:text-red-500 focus:bg-red-100"
+                  : "bg-slate-50/50 border-slate-200/70 focus:bg-slate-50/20 focus:border-slate-300"
+              } p-4 uppercase border placeholder:capitalize outline-none hover:border-slate-200 rounded-xl w-full max-w-sm transition-all`}
+              {...register("identityNumber")}
+              placeholder="Ad ve Soyad"
+              autoComplete="off"
+              maxLength={11}
+            />
+            <span className="block mt-1.5 text-sm md:text-base text-red-600">
+              {errors.identityNumber?.message}
             </span>
           </label>
         </form>
--- app/utils/yup.js
+++ app/utils/yup.js
@@ -1,5 +1,7 @@
 import * as yup from "yup";
 
-export const trafik = yup.object().shape({
+export const TrafikSchema = yup.object().shape({
   fullName: yup.string().required("Bu alan boş bırakılamaz."),
+  identityNumber: yup.string().required("Bu alan boş bırakılamaz.").min(11, "11 haneli kimlik numaranızı yazınız."),
 });
+      
